809dd3a743ef3bc1cffcb1679a43869af15fec3e
Katy <mxb.kativa@gmail.com>
2021-07-26 19:01:02 +0300

    nav and jumbotron try-out

--- bnb/src/app/app.module.ts
+++ bnb/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 import {MaterialModule} from './material.module';
 
 @NgModule({
@@ -14,7 +15,7 @@ import {MaterialModule} from './material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
